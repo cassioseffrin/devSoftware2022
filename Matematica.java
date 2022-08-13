3590ae88fa3cc29c47aaa507358f059dce905d97
Cassio Seffrin <cassioseffrin@gmail.com>
2022-08-12 21:36:16 -0300

    tudo

---
+++ Matematica.java
@@ -0,0 +1,7 @@
+
+class Matematica {
+        int multiplicar(int a, int b){
+            return a*b;
+        }
+}
+
---
+++ Principal.java
@@ -0,0 +1,9 @@
+class Principal{
+
+	public static void main(String a[]){
+		Matematica m = new Matematica();
+		int resultado = m.multiplicar(2,4);
+		System.out.println("resultado: "+resultado);
+	}
+
+} 
