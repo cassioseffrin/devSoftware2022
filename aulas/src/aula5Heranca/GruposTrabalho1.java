9e4fe33e58e744dd3a125cc90b0f32b78153a9e8
Cassio Seffrin <cassioseffrin@gmail.com>
2022-10-21 20:16:17 -0300

    algoritmo

--- aulas/src/aula5Heranca/GruposTrabalho1.java
+++ aulas/src/aula5Heranca/GruposTrabalho1.java
@@ -1,185 +1,60 @@
 package aula5Heranca;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
-
+import java.util.stream.Stream;
 public class GruposTrabalho1 {
+	
+	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String args[]) {
-
-//		ArrayList<String> listaAlunos = pegarListaAlunos();
-		
-//		ArrayList<String> listaAlunosSorteados = new ArrayList<>();
-//		ArrayList<Integer> indices = new ArrayList<>();
-//		for (int i = 0; i <= 34; i++) {
-//			int nAleatorio = sorteiaAluno(indices);
-//			listaAlunosSorteados.add(listaAlunos.get(nAleatorio));
-//			indices.add(nAleatorio);
-//			System.out.println(listaAlunos.get(nAleatorio));	
-//		}
-		
-//		Collections.shuffle(listaAlunos);
-//		for (int j = 0; j < 7; j++) {
-//			int inicio = j * 5;
-//			System.out.println("***** Grupo " + (j + 1) + " *****");
-//			for (int i = inicio; i <= inicio + 4; i++) {
-//				System.out.println(i + " - " + listaAlunos.get(i));
-//			}
-//		}
-		
+		ArrayList<String> listaAlunos = pegarListaAlunos();
+		ArrayList<String> listaAlunosSorteados = new ArrayList<>();
+		ArrayList<Integer> indices = new ArrayList<>();
+		for (int i = 0; i < listaAlunos.size(); i++) {
+			int nAleatorio = sorteiaAluno(indices, listaAlunos.size());
+			listaAlunosSorteados.add(listaAlunos.get(nAleatorio));
+			indices.add(nAleatorio);
+//			System.out.println(listaAlunos.get(nAleatorio));
+		}
+//		System.out.print(indices);
 		ArrayList<String> listaTemas = pegarListaTemas();
 		Collections.shuffle(listaTemas);
 		for (int j = 0; j < 7; j++) {
- 
-			System.out.println("***** Grupo " + (j + 1) + " *****");
-	 
-				System.out.println(  listaTemas.get(j));
-		 
-		}
-		
-
-//		CONCORDIA
-//		
-//		1	CAUÃ FILIPE GRAFF					
-//		2	ERIK KNOBLAUCH RICKWARDT					
-//		3	FELIPE DO AMARAL ANTONIAK					
-//		4	FELIPE GABRIEL HINKEL					
-//		5	GUILHERME GABRIEL CAMBRI FERRI					
-//		6	IGOR RENOSTO TONDELLO					
-//		7	JOÃO PEDRO WEBBER					
-//		8	LÁZARO PIOLA SCHAURICH					
-//		9	LUCAS EDUARDO VIVAN					
-//		10	NIKOLAS SILVA LUNKES					
-//		11	RICARDO BONISSONI ROSSETTO					
-//		12	WESLEY FICANHA WUNDER
-//		
-//		
-//		CURIBANOS
-//		1	DIOGO GUILHERME ROLOFF					
-//		2	FELIPE AGUIAR					
-//		3	IGOR JOSÉ XAVIER					
-//		4	ISAAC ANTONIO GUISOLPHI FILHO					
-//		5	LUIS FELIPE RIBEIRO					
-//		6	MATHEUS DA SILVA STEFFEN VIEIRA					
-//		7	RUAN VICTOR SCHEIDT					
-//		8	WESLEY EVANDRO PEREIRA
-//		
-//		
-//		MAFRA
-//		1	ANA BEATRIZ SCHNEIDER					
-//		2	ANDRÉ VITOR VALERIO					
-//		3	ARTHUR HEUKO CIBOK					
-//		4	BRUNO EDUARDO CASSIAS BARUZZO					
-//		5	ELESSANDER RIBOVSKI					
-//		6	GABRIELI DE FÁTIMA RUTHES					
-//		7	GIOVANE RODRIGUES DA LUZ					
-//		8	LUCAS GABRIEL PSCHEIDT					
-//		9	LUCAS SKIBINSKI					
-//		10	LUIS FELIPE SQUIOQUET CAMILLO					
-//		11	MARIANA SAUER					
-//		12	MATHEUS RUCHINSKI SPERKOSKI					
-//		13	NICOLAS CARDOSO VILHA DO LAGO					
-//		14	PAULO EDESON GONTARSKI JUNIOR					
-//		15	RUAN FELIPE DE LIMA
-
-//		CONCORDIA
-//		
-//		1	CAUÃ FILIPE GRAFF					
-//		2	ERIK KNOBLAUCH RICKWARDT					
-//		3	FELIPE DO AMARAL ANTONIAK					
-//		4	FELIPE GABRIEL HINKEL					
-//		5	GUILHERME GABRIEL CAMBRI FERRI					
-//		6	IGOR RENOSTO TONDELLO					
-//		7	JOÃO PEDRO WEBBER					
-//		8	LÁZARO PIOLA SCHAURICH					
-//		9	LUCAS EDUARDO VIVAN					
-//		10	NIKOLAS SILVA LUNKES					
-//		11	RICARDO BONISSONI ROSSETTO					
-//		12	WESLEY FICANHA WUNDER
-//		
-//		
-//		CURIBANOS
-//		1	DIOGO GUILHERME ROLOFF					
-//		2	FELIPE AGUIAR					
-//		3	IGOR JOSÉ XAVIER					
-//		4	ISAAC ANTONIO GUISOLPHI FILHO					
-//		5	LUIS FELIPE RIBEIRO					
-//		6	MATHEUS DA SILVA STEFFEN VIEIRA					
-//		7	RUAN VICTOR SCHEIDT					
-//		8	WESLEY EVANDRO PEREIRA
-//		
-//		
-//		MAFRA
-//		1	ANA BEATRIZ SCHNEIDER					
-//		2	ANDRÉ VITOR VALERIO					
-//		3	ARTHUR HEUKO CIBOK					
-//		4	BRUNO EDUARDO CASSIAS BARUZZO					
-//		5	ELESSANDER RIBOVSKI					
-//		6	GABRIELI DE FÁTIMA RUTHES					
-//		7	GIOVANE RODRIGUES DA LUZ					
-//		8	LUCAS GABRIEL PSCHEIDT					
-//		9	LUCAS SKIBINSKI					
-//		10	LUIS FELIPE SQUIOQUET CAMILLO					
-//		11	MARIANA SAUER					
-//		12	MATHEUS RUCHINSKI SPERKOSKI					
-//		13	NICOLAS CARDOSO VILHA DO LAGO					
-//		14	PAULO EDESON GONTARSKI JUNIOR					
-//		15	RUAN FELIPE DE LIMA
-
-	}
-
-	private static int sorteiaAluno(ArrayList<Integer> indices) {
-				
-		Random r = new Random();
-		int numero = r.nextInt(34);
- 
-		boolean found = true;
-
-		  while(found==false ){
-			for (int i = 0; i < indices.size(); i++) {	
-				if (numero == indices.get(i)) {					
-					found = true;
-				}
-			}	
-			if (found==true) {
-				numero = r.nextInt(34);
-			}else {
-				found=false;
-			
+			int inicio = j * 5;
+			System.out.println("***** Grupo " + listaTemas.get(j) + " *****");
+			for (int i = inicio; i <= inicio + 4; i++) {
+				System.out.println(i + " - " + listaAlunos.get(i));
 			}
-			
 		}
-		return numero;
 	}
-		
-//		Random r = new Random();
-//		int numero = r.nextInt(34);
-//		int numeroFinal = numero;
-//		boolean sortearNovamente = false;
-////		if (indices.get(i) != numero) {
-////			numeroFinal = numero;
-////			break;
-////		} else {
-////			numeroFinal = sorteiaAluno(indices);
-////		}
-//		
-//		for (int i = 0; i < indices.size(); i++) {	
-//			if ( numero == indices.get(i)) {
-//				sortearNovamente = true;
-//				break;
-//			}else {
-//				return numero;
-//			}
-//		}
-//		
-//		if (sortearNovamente) {
-//			return  sorteiaAluno(indices);
-//		}else {
-//			return numero;
-//		}
-		
-//		return numeroFinal;
 
+	/**
+	 * 
+	 * @param indices
+	 * @param tamanho
+	 * @return retorna um numero aleatorio
+	 * 
+	 */
+	private static int sorteiaAluno(ArrayList<Integer> indices, int tamanho) {
+		Random r = new Random();
+		Integer numero = r.nextInt(tamanho);
+		Stream<Integer> filter = indices.stream().filter(i -> i.intValue() == numero);
+		if (filter.count() > 0) {
+			return sorteiaAluno(indices, tamanho);
+		} else {
+
+			return numero;
+		}
+	}
+
+	/**
+	 * 
+	 * @return retorna a lista de todos alunos
+	 */
 
 	private static ArrayList<String> pegarListaAlunos() {
 
@@ -223,9 +98,11 @@ public class GruposTrabalho1 {
 		return listaAlunos;
 
 	}
-	
-	
-	
+
+	/**
+	 * 
+	 * @return retorna a lista de grupos
+	 */
 	private static ArrayList<String> pegarListaTemas() {
 
 		ArrayList<String> temas = new ArrayList<>();
@@ -236,15 +113,16 @@ public class GruposTrabalho1 {
 		temas.add("HOSPITAL");
 		temas.add("TRANSPORTADORA");
 		temas.add("RESTAURANTE");
-//		listaAlunos.add("OP1 E-COMMERCE");
-//		listaAlunos.add("OP2 BIBLIOTECA");
-//		listaAlunos.add("OP3 SISTEMA ACADEMICO");
-//		listaAlunos.add("OP4 LOCADORA");
-//		listaAlunos.add("OP5 SISTEMA PARA CONSULTORIOS");
-//		listaAlunos.add("OP5 SISTEMA PARA ESCRITORIOS DE ADVOCACIA");
-		
+
+		temas.add("OP1 E-COMMERCE");
+		temas.add("OP2 BIBLIOTECA");
+		temas.add("OP3 SISTEMA ACADEMICO");
+		temas.add("OP4 LOCADORA");
+		temas.add("OP5 SISTEMA PARA CONSULTORIOS");
+		temas.add("OP5 SISTEMA PARA ESCRITORIOS DE ADVOCACIA");
+
 		return temas;
-		
+
 	}
 
 }
